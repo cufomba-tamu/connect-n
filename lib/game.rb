2c57f13dbd42cb618b3d4dcc402057439390f76d
cufomba-tamu <cufomba@tamu.edu>
2026-09-11 12:19:19 -0500

    Add Game#ask_column with quit handling

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,15 @@
 # frozen_string_literal: true
 
+class Game
+  def ask_column(player)
+    print "#{player.name}, choose a column, or 'q' to quit: "
+    raw = gets.strip
+
+    if raw.downcase == 'q'
+      puts 'Thanks for playing!'
+      exit(0)
+    end
+
+    raw
+  end
+end

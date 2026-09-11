0af8281a8bc2cb428f6c3d7257a54728d0f03996
Michael Zhou <michaelzhouwp@gmail.com>
2026-09-11 01:35:42 -0500

    add initial Board class and tests

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,9 @@
 # frozen_string_literal: true
+class Board
+  attr_accessor :rows, :columns  # we create the getter and setter methods
 
+  def initialize(rows, columns)
+    @rows = rows  # board remembers the rows and columns requested
+    @columns = columns
+  end
+end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,2 +1,19 @@
 # frozen_string_literal: true
 
+require_relative "../lib/board"
+
+describe Board do  # testing the Board class using rspec
+  it "stores the number of rows" do
+    board = Board.new(6, 7) # we create a Board object with 6 rolls and 7 columns
+    expect(board.rows).to eq(6) # expect number of rows to equal 6
+  end
+
+  it "stores the number of columns" do
+    board = Board.new(6, 7)
+    expect(board.columns).to eq(7)
+  end
+
+end
+
+
+# test command - bundle exec rspec spec/board_spec.rb

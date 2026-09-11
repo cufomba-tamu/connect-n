8a4885bfd0b7b4c3dcc56d313e9eed79cf4bdea0
Michael Zhou <michaelzhouwp@gmail.com>
2026-09-11 12:54:16 -0500

    create and display the board in the terminal

--- main.rb
+++ main.rb
@@ -1,1 +1,6 @@
-puts 'Welcome to Connect N Game'
+#puts 'Welcome to Connect N Game'
+
+require_relative "lib/board"
+
+board = Board.new(6, 7)
+board.display
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -13,6 +13,25 @@ describe Board do  # testing the Board class using rspec
     expect(board.columns).to eq(7)
   end
 
+  it "creates a grid with the correct number of rows" do
+    board = Board.new(6, 7)
+    expect(board.grid.length).to eq(6) # should contain 6 rows
+  end
+
+  it "creates a grid with the correct number of columns" do
+    board = Board.new(6, 7)
+    expect(board.grid[0].length).to eq(7) # to check the number rows starting from index [0]
+                                          # length checks the amount of columns in that row
+  end
+
+  it "displays the board" do
+    board = Board.new(2, 3)  # creates a 2x3 board.. its simpler to test this case than a bigger 6x7 board
+
+    # runs the board display and checks what is printed to the terminal
+    expect {board.display_board}.to output("| | | |\n| | | |\n").to_stdout # to std_out means we expect
+                                                                          # this outcome to be printed to the terminal
+  end
+
 end
 
 

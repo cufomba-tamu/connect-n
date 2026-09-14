d4f17c50ec8e21c89e385726383f50c3f2848419
Michael Zhou <michaelzhouwp@gmail.com>
2026-09-14 10:42:47 -0500

    Update board_spec.rb

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,7 +4,7 @@ require_relative "../lib/board"
 
 describe Board do  # testing the Board class using rspec
   it "stores the number of rows" do
-    board = Board.new(6, 7) # we create a Board object with 6 rolls and 7 columns
+    board = Board.new(6, 7) # we create a Board object with 6 rows and 7 columns
     expect(board.rows).to eq(6) # expect number of rows to equal 6
   end
 

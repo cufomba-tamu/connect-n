55e3f34e56bfb0880f41ebdd7de8eac324c1bf0e
Michael Zhou <michaelzhouwp@gmail.com>
2026-09-22 23:09:02 -0500

    completed board, scoreboard and the tests

--- main.rb
+++ main.rb
@@ -1,14 +1,2 @@
-#puts 'Welcome to Connect N Game'
-
 require_relative "lib/board"
-
-board = Board.new(6, 7)
-
-# drop pieces into the board
-board.drop_piece(2, "R")
-board.drop_piece(2, "B")
-board.drop_piece(3, "R")
-board.drop_piece(4, "B")
-
-# display the board
-board.display_board # displays the board in the terminal
+require_relative "lib/scoreboard"

980184586f32390ab2e2527976df565595cc96a1
Michael Zhou <michaelzhouwp@gmail.com>
2026-09-22 23:12:28 -0500

    Commit changes to main

--- main.rb
+++ main.rb
@@ -1,2 +1,12 @@
 require_relative "lib/board"
-require_relative "lib/scoreboard"
+
+board = Board.new(6, 7)
+
+# drop pieces into the board
+board.drop_piece(2, "R")
+board.drop_piece(2, "B")
+board.drop_piece(3, "R")
+board.drop_piece(4, "B")
+
+# display the board
+board.display_board # displays the board in the terminal

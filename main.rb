caed1669e3d343c67567a127f18febdd3541c251
Michael Zhou <michaelzhouwp@gmail.com>
2026-09-22 23:16:59 -0500

    Check the scoreboard display in the terminal

--- main.rb
+++ main.rb
@@ -10,3 +10,23 @@ board.drop_piece(4, "B")
 
 # display the board
 board.display_board # displays the board in the terminal
+
+#############################################
+
+# display the player scores in the terminal
+scoreboard = ScoreBoard.new
+
+puts "Starting Score:" # Display the starting score
+scoreboard.display
+
+scoreboard.add_player1_win # give player 1 a win
+
+# display the updated score
+puts "\nAfter Player 1 wins:"
+scoreboard.display
+
+scoreboard.add_player2_win # give player 2 a win
+
+# display the updated score
+puts "\nAfter Player 2 wins:"
+scoreboard.display
